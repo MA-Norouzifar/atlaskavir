ff34cf3352f9e4e818278f4a00a828192ef30668
M-A-D <info@pythoniran.ir>
2020-10-17 14:09:33 +0330

    second all code

---
+++ frontend/build/precache-manifest.e8e939dac7ea9c8611ab74e9d3d7970d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13881355af1b83975dd8c24539d49b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19a7bd6496126e8dd614",
+    "url": "/static/css/main.e85d9d01.chunk.css"
+  },
+  {
+    "revision": "1f4eba3b17abb4692b42",
+    "url": "/static/js/2.b07c0553.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b07c0553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a7bd6496126e8dd614",
+    "url": "/static/js/main.640a027e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "249bebb8b35d10485ab0c9a9149bfaed",
+    "url": "/static/media/cars.249bebb8.jpg"
+  }
+]);
